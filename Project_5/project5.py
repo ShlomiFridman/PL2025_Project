d35d3aeb7c7c6b3fc387727c2b41e08e589cb108
DanielFeldman1 <daniel.feldman303@gmail.com>
2026-01-02 16:35:07 +0200

    refactor: change find_bk loop and add calc norm function

--- Project_5/project5.py
+++ Project_5/project5.py
@@ -7,50 +7,49 @@ scale_pow = 16
 mat = np.loadtxt("exp_data.csv", delimiter=",")
 
 n = mat.shape[0]
-    
-def calc_vec_norm(vec: np.array) -> float:
+
+def calc_vec_norm(vec):
     return np.sqrt(np.sum(np.square(vec)))
 
-def find_bk(A: np.array) -> np.array:
-    k=1
-    b_curr = np.ones(n)
-    b_next = np.zeros(n)
-    while True:
+def find_bk(A):
+    b_curr = np.zeros(n)
+    b_next = np.ones(n)
+    while calc_vec_norm(b_next - b_curr) >= epsilon:
+        b_curr = b_next
         b_next = A @ b_curr
         b_next /= calc_vec_norm(b_next)
-        
-        if calc_vec_norm(b_next - b_curr) < epsilon:
-            return b_curr
-        else:
-            b_curr = b_next
-        
-def find_m(A: np.array, bk: np.array) -> int:
+    return b_curr
+
+def calc_mat_norm(A, bk):
     bk_t = bk.T
     mu_org = (bk_t @ A @ bk) / (np.dot(bk_t, bk))
+    return mu_org
+
+def find_m(A, bk):
+    mu_org = calc_mat_norm(A, bk)
     mu_acc = mu_org
     m = 1
     m_fact = 1
-    
+
     while (mu_acc / m_fact) >= epsilon:
          mu_acc *= mu_org
          m += 1
          m_fact *= m
-         
+
     return m
 
-def calc_exp_mat(A: np.array, m: int) -> np.array:
+def calc_exp_mat(A, m):
     A_acc = np.eye(n, dtype=A.dtype)
     res_mat = np.eye(n, dtype=A.dtype)
     k_fact = 1
-    
+
     for i in range(1, m+1):
         A_acc = A_acc @ A
         k_fact *= i
         res_mat += A_acc/k_fact
-    
     return res_mat
 
-def run_prog(A: np.array) -> tuple[np.array, float]:
+def run_prog(A):
     startTime = time.time()
     
     A_scaled = A/(2**scale_pow)
@@ -67,6 +66,6 @@ def run_prog(A: np.array) -> tuple[np.array, float]:
 
 
 res_mat, total_time = run_prog(mat)
-print(f"Total time: {total_time:.3f} seconds")
-print("Result sub-matrix (5x5):")
+print(f"Before:\n{mat[:5,:5]}\n")
+print(f"Total time: {total_time:.3f} seconds\n")
 print(res_mat[:5,:5])
